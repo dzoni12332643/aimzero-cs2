213145d1fe20fc2a61702e3fae9621a217fe9202
dzoni12332643 <nikolaspasojevvic123@gmail.com>
2024-07-19 17:00:12 +0200

    Add files via upload

--- gui/menu.cpp
+++ gui/menu.cpp
@@ -293,9 +293,8 @@ void imGuiMenu::aboutMeRender() {
 	if (tabCount == 5) {
 		ImGui::BeginChild("UPDATES", ImVec2(0, 0), true);
 		ImGui::PushFont(imGuiMenu::titleText);
-		ImGui::Text("[+] Fixed for latest game update");
-		ImGui::Text("[+] Added Visibility check for esp");
-		ImGui::Text("[+] Added hitboxes for ragebot");
+		ImGui::Text("[+] Fixed some things in menu");
+		ImGui::Text("[+] Added keybind to close the cheat >END<");
 		ImGui::PopFont();
 		ImGui::Text("");
 		ImGui::Dummy(ImVec2(0.0f, textSeparatorSpace));
